d40b2c8d597b23447578b89b8d2d30a47f05e864
Musvol <Musvol>
2018-09-17 14:31:43 +0000

    add constraint for room size

--- openacademy/models.py
+++ openacademy/models.py
@@ -16,4 +16,16 @@ class Sessions(models.Model):
     user_id = fields.Many2one('res.users', string="Instructor")
     start_date = fields.Date()
     seats = fields.Integer('Room Capacity')
+    attendee_cnt = fields.Integer(compute="get_attendee_count")
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+    @api.depends('attendee_ids')
+    def get_attendee_count(self):
+        for record in self:
+            record.attendee_cnt = len(record.attendee_ids)
+
+    @api.constrains('attendee_cnt', 'seats')
+    def _check_description(self):
+        for record in self:
+            if record.attendee_cnt > record.seats:
+                raise ValidationError("No seat left in the classroom.")
