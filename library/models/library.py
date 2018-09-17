cacbc26c45cfe6100a02609930c15d2063786c68
Lucas Lefèvre <lul@odoo.com>
2018-09-17 13:19:05 +0000

    library

---
+++ library/models/library.py
@@ -0,0 +1,32 @@
+from odoo import api, fields, models
+
+class Book(models.Model):
+    
+    _name = 'library.book'
+    name = fields.Char('Title')
+    isbn = fields.Char('ISBN')
+    author_ids = fields.Many2many('library.author')
+    loan_ids = fields.One2many('library.loan', 'book_id')
+    
+    
+class Loan(models.Model):
+    _name = 'library.loan'
+    book_id = fields.Many2one('library.book')
+    customer_id = fields.Many2one('library.customer')
+    return_date = fields.Date()
+    
+class Author(models.Model)
+
+    _name = 'library.author'
+    name = fields.Char()
+    book_ids = fields.Many2many('library.book')
+
+    
+class Customer(models.Model):
+    _name = 'library.customer'
+    _inherit = 'res.partner'
+    loan_ids = fields.One2many('library.loan', 'customer_id')
+    
+    
+    
+    
--- openacademy/models/academy.py
+++ openacademy/models/academy.py
@@ -4,20 +4,22 @@ class Course(models.Model):
     _description = 'Course'
     _name = 'openacademy.course'
     name = fields.Char()
-    level = fields.Selection((0, "Easy"), (1, "Normal"))
-    responsible = fields.One2Many('openacademy.trainer')
+    level = fields.Selection([(0, "Easy"), (1, "Normal")], default=0)
+    trainer_id = fields.Many2one('openacademy.trainer')
+    
+    sessions = fields.Many2one('openacademy.session')
     
 class Trainer(models.Model):
     _description = 'Trainer'
     _name = 'openacademy.trainer'
-    
-    sessions = fields.One2many('openacademy.session', string="Sessions")
+    sessions = fields.One2many('openacademy.session', 'trainer_id',  string="Sessions")
+    responsible_for = fields.One2many('openacademy.course', 'trainer_id')
     
 class Student(models.Model):
     _description = 'Student'
     _name = 'openacademy.student'
     
-    sessions = fields.Many2many('openacademy.session', string='Student')
+    sessions = fields.Many2many('openacademy.session', string='Sessions')
     
     
     
@@ -28,6 +30,8 @@ class Session(models.Model):
     date = fields.Date("Day of training")
     course_id = fields.Many2one('openacademy.course', string='Course')
     trainer_id = fields.Many2one('openacademy.trainer', string="Trainer")
+    
+    students = fields.Many2many('openacademy.student', string='Students')
     
 
     
