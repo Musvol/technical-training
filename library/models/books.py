927001e58ee28b7b8de1723cb73883ab09398848
Musvol <Musvol>
2018-09-18 12:32:44 +0000

    delegation inheritance

--- library/models/books.py
+++ library/models/books.py
@@ -2,7 +2,7 @@
 from odoo import models, fields
 
 class Books(models.Model):
-    _name = 'library.book'
+    _name = 'library.bookref'
     _description = 'Book'
 
     name = fields.Char(string='Title')
@@ -10,4 +10,18 @@ class Books(models.Model):
     edition_date =  fields.Date(string='Edition date',)
     isbn = fields.Char(string='ISBN')
     publisher_id = fields.Many2one('library.publisher', string='Publisher')
+    
+    
+    
+class BookCopies(models.Model):
+    _name = 'library.book.copy'
+    _description = 'Specific copy of a book'
+    
+    _inherits = {
+        'library.bookref': 'book_id',
+    }
+
+    book_id = fields.Many2one('library.bookref', required=True, ondelete='cascade')
     rental_ids = fields.One2many('library.rental', 'book_id', string='Rentals')
+    
+    
